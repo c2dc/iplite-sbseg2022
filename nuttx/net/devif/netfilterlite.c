9d6c020c3e25a0b8c179ea45b378616a5fbe7dd2
P1F <rtsouza15@gmail.com>
2022-05-07 21:09:59 -0300

    move function & assign ptr to buf

--- nuttx/net/devif/netfilterlite.c
+++ nuttx/net/devif/netfilterlite.c
@@ -48,6 +48,28 @@ bool netfilterlite_addrule(int rule, in_addr_t srcipaddr, in_addr_t destipaddr, 
     return true;
 }
 
+
+/* Get a pointer to the TCP header.  The TCP header lies just after the
+ * the link layer header and the IP header.
+*/
+FAR struct tcp_hdr_s* get_tcp_hdr_from_ipv4(FAR struct ipv4_hdr_s *buf) {
+    uint16_t iphdrlen;
+    FAR struct tcp_hdr_s *tcp;
+    FAR struct ipv4_hdr_s *ptr; // auxiliar pointer
+
+    /* Get the IP header length  */
+    iphdrlen = (buf->vhl & IPv4_HLMASK) << 2;
+
+    /* Go with ptr from ipv4 to tcp offset */
+    ptr = buf;
+    ptr += iphdrlen;
+
+    /* Cast ptr to tcp header */
+    tcp = (FAR struct tcp_hdr_s *) ptr;
+    
+    return tcp;
+}
+
 // TODO: code all missing verifications
 bool netfilterlite_verify_ipv4(FAR struct ipv4_hdr_s *buf) {
     FAR struct ipv4_hdr_s *ipv4;
@@ -78,24 +100,4 @@ bool netfilterlite_verify_ipv4(FAR struct ipv4_hdr_s *buf) {
     }
 
     return false;
-}
-
-/* Get a pointer to the TCP header.  The TCP header lies just after the
- * the link layer header and the IP header.
-*/
-FAR struct tcp_hdr_s* get_tcp_hdr_from_ipv4(FAR struct ipv4_hdr_s *buf) {
-    uint16_t iphdrlen;
-    FAR struct tcp_hdr_s *tcp;
-    FAR struct ipv4_hdr_s *ptr; // auxiliar pointer
-
-    /* Get the IP header length  */
-    iphdrlen = (buf->vhl & IPv4_HLMASK) << 2;
-
-    /* Go with ptr from ipv4 to tcp offset */
-    ptr += iphdrlen;
-
-    /* Cast ptr to tcp header */
-    tcp = (FAR struct tcp_hdr_s *) ptr;
-    
-    return tcp;
 }
