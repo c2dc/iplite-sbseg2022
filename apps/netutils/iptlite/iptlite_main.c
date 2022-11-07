f271b425930827d533b47a4b5906304c0233dd1d
P1F <rtsouza15@gmail.com>
2022-11-06 21:01:02 -0300

    fix checkpatch issues

--- apps/netutils/iptlite/iptlite_main.c
+++ apps/netutils/iptlite/iptlite_main.c
@@ -27,14 +27,14 @@ void listall_rules()
   "ID", "RULE", "SRC IPADDR", "DEST IPADDR", "SRC PORT", "DEST PORT");
 
   for (int i = 0; i < rules_counter; i++)
-  {
-    for (int j = 0; j < RULE_INFO_MAX_SIZE; j++)
     {
-      printf("%c", table[i][j]);
-    }
+      for (int j = 0; j < RULE_INFO_MAX_SIZE; j++)
+        {
+          printf("%c", table[i][j]);
+        }
 
-    printf("\n");
-  }
+      printf("\n");
+    }
 }
 
 /****************************************************************************
@@ -49,30 +49,30 @@ int main(int argc, FAR char *argv[])
   bool rule_added;
 
   if (argc < 2)
-  {
-    printf("Not enough arguments!\n");
-    return -1;
-  }
+    {
+      printf("Not enough arguments!\n");
+      return -1;
+    }
 
   if (strcmp(argv[1], "DROP") == 0 && argc == 6)
     rule = 0;
   else if (strcmp(argv[1], "FLUSHALL") == 0 && argc == 2)
-  {
-    rule = 1;
-    nflite_flushall();
-    return 0;
-  }
+    {
+      rule = 1;
+      nflite_flushall();
+      return 0;
+    }
   else if (strcmp(argv[1], "LISTALL") == 0 && argc == 2)
-  {
-    rule = 2;
-    listall_rules();
-    return 0;
-  }
+    {
+      rule = 2;
+      listall_rules();
+      return 0;
+    }
   else
-  {
-    printf("Invalid command! Verify command pattern.\n");
-    return -1;
-  }
+    {
+      printf("Invalid command! Verify command pattern.\n");
+      return -1;
+    }
 
   inet_pton(AF_INET, argv[2], &srcipaddr);
   inet_pton(AF_INET, argv[3], &destipaddr);
--- nuttx/net/devif/nflite.c
+++ nuttx/net/devif/nflite.c
@@ -1,4 +1,9 @@
 /****************************************************************************
+ * net/devif/nflite.c
+ * netfilter lite packet filter modules
+ ****************************************************************************/
+
+/****************************************************************************
  * Included Files
  ****************************************************************************/
 
@@ -14,167 +19,177 @@
 
 typedef enum rules
 {
-    DROP,
-    FLUSHALL,
-    LISTALL
+  DROP,
+  FLUSHALL,
+  LISTALL
 } rules;
 
 typedef struct chain chain;
 
 struct chain
 {
-    rules     rule;
-    in_addr_t srcipaddr;
-    in_addr_t destipaddr;
-    in_port_t srcport;
-    in_port_t destport;
-    chain     *next;
+  rules rule;
+  in_addr_t srcipaddr;
+  in_addr_t destipaddr;
+  in_port_t srcport;
+  in_port_t destport;
+  chain *next;
 };
 
 chain *chain_head;
 chain *last_rule;
 int rules_counter;
 
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
 void nflite_initialize(void)
 {
-    chain_head = (chain *) malloc(sizeof(chain));
-    chain_head->next = NULL;
-    rules_counter = 0;
+  chain_head = (chain *)malloc(sizeof(chain));
+  chain_head->next = NULL;
+  rules_counter = 0;
 
-    last_rule = chain_head;
+  last_rule = chain_head;
 }
 
 bool nflite_addrule(int rule, in_addr_t srcipaddr, in_addr_t destipaddr,
-                            in_port_t srcport, in_port_t destport)
-                            {
-    chain *new_chainrule = (chain *) malloc(sizeof(chain));
-    if (new_chainrule == NULL)
-        return false;
+                    in_port_t srcport, in_port_t destport)
+{
+  chain *new_chainrule = (chain *)malloc(sizeof(chain));
+  if (new_chainrule == NULL) return false;
 
-    new_chainrule->rule = rule;
-    new_chainrule->srcipaddr = srcipaddr;
-    new_chainrule->destipaddr = destipaddr;
-    new_chainrule->srcport = srcport;
-    new_chainrule->destport = destport;
-    new_chainrule->next = NULL;
+  new_chainrule->rule = rule;
+  new_chainrule->srcipaddr = srcipaddr;
+  new_chainrule->destipaddr = destipaddr;
+  new_chainrule->srcport = srcport;
+  new_chainrule->destport = destport;
+  new_chainrule->next = NULL;
 
-    last_rule->next = new_chainrule;
-    last_rule = last_rule->next;
-    rules_counter++;
+  last_rule->next = new_chainrule;
+  last_rule = last_rule->next;
+  rules_counter++;
 
-    return true;
+  return true;
 }
 
 bool nflite_verify_ipv4(FAR struct net_driver_s *dev)
 {
-    FAR struct ipv4_hdr_s *ipv4;
-    FAR struct tcp_hdr_s *tcp;
-    in_addr_t destipaddr;
-    in_addr_t srcipaddr;
-    in_port_t srcport;
-    in_port_t destport;
-    uint16_t iphdrlen;
+  FAR struct ipv4_hdr_s *ipv4;
+  FAR struct tcp_hdr_s *tcp;
+  in_addr_t destipaddr;
+  in_addr_t srcipaddr;
+  in_port_t srcport;
+  in_port_t destport;
+  uint16_t iphdrlen;
 
-    ipv4 = ((FAR struct ipv4_hdr_s *)&dev->d_buf[NET_LL_HDRLEN(dev)]);
-    iphdrlen = (ipv4->vhl & IPv4_HLMASK) << 2;
-    tcp = (FAR struct tcp_hdr_s *)&dev->d_buf[iphdrlen + NET_LL_HDRLEN(dev)];
+  ipv4 = ((FAR struct ipv4_hdr_s *)&dev->d_buf[NET_LL_HDRLEN(dev)]);
+  iphdrlen = (ipv4->vhl & IPv4_HLMASK) << 2;
+  tcp = (FAR struct tcp_hdr_s *)&dev->d_buf[iphdrlen + NET_LL_HDRLEN(dev)];
 
-    destipaddr = net_ip4addr_conv32(ipv4->destipaddr);
-    srcipaddr = net_ip4addr_conv32(ipv4->srcipaddr);
-    srcport = tcp->srcport;
-    destport = tcp->destport;
+  destipaddr = net_ip4addr_conv32(ipv4->destipaddr);
+  srcipaddr = net_ip4addr_conv32(ipv4->srcipaddr);
+  srcport = tcp->srcport;
+  destport = tcp->destport;
 
-    chain *current_rule = chain_head->next;
-    while (current_rule)
+  chain *current_rule = chain_head->next;
+  while (current_rule)
     {
-        /* Verify incoming tuple */
+      /* Verify incoming tuple */
 
-        if ((current_rule->destipaddr == 0 \
-        || current_rule->destipaddr == destipaddr) \
-        && (current_rule->srcipaddr == 0 \
-        || current_rule->srcipaddr == srcipaddr) \
-        && (current_rule->destport == 0 \
-        || current_rule->destport == destport) \
-        && (current_rule->srcport == 0 \
-        || current_rule->srcport == srcport))
-            return false;
-        current_rule = current_rule->next;
+      if ((current_rule->destipaddr == 0 \
+      || current_rule->destipaddr == destipaddr) \
+      && (current_rule->srcipaddr == 0 \
+      || current_rule->srcipaddr == srcipaddr) \
+      && (current_rule->destport == 0 \
+      || current_rule->destport == destport) \
+      && (current_rule->srcport == 0 \
+      || current_rule->srcport == srcport)) return false;
+
+      current_rule = current_rule->next;
     }
 
-    return true;
+  return true;
 }
 
 void nflite_flushall(void)
 {
-    chain *current_rule = chain_head->next;
-    chain *head = chain_head->next;
-    while (head != NULL)
+  chain *current_rule = chain_head->next;
+  chain *head = chain_head->next;
+  while (head != NULL)
     {
-        current_rule = head;
-        head = head->next;
-        free(current_rule);
+      current_rule = head;
+      head = head->next;
+      free(current_rule);
     }
 
-    chain_head->next = NULL;
-    rules_counter = 0;
+  chain_head->next = NULL;
+  rules_counter = 0;
 
-    last_rule = chain_head;
+  last_rule = chain_head;
 }
 
-char * get_rule_name(rules rule)
+char **nflite_listall(void)
 {
-    switch (rule)
+  chain *head = chain_head->next;
+  char **table = (char **)malloc(rules_counter * sizeof(char *));
+
+  for (int i = 0; i < rules_counter; i++)
     {
-        case DROP: return "DROP";
-        case FLUSHALL: return "FLUSHALL";
-        case LISTALL: return "LISTALL";
+      table[i] = (char *)malloc(RULE_INFO_MAX_SIZE * sizeof(char));
     }
 
-    return "UNDEFINED";
+  int idx = 0;
+  while (head != NULL)
+    {
+      get_rule_info(head, table, idx);
+      head = head->next;
+      idx++;
+    }
+
+  return table;
+}
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+char *get_rule_name(rules rule)
+{
+  switch (rule)
+  {
+  case DROP:
+    return "DROP";
+  case FLUSHALL:
+    return "FLUSHALL";
+  case LISTALL:
+    return "LISTALL";
+  }
+
+  return "UNDEFINED";
 }
 
 void get_rule_info(chain *node, char **table, int idx)
 {
-    char srcipaddr[INET_ADDRSTRLEN];
-    char destipaddr[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &node->srcipaddr, srcipaddr, INET_ADDRSTRLEN);
-    inet_ntop(AF_INET, &node->destipaddr, destipaddr, INET_ADDRSTRLEN);
+  char srcipaddr[INET_ADDRSTRLEN];
+  char destipaddr[INET_ADDRSTRLEN];
+  inet_ntop(AF_INET, &node->srcipaddr, srcipaddr, INET_ADDRSTRLEN);
+  inet_ntop(AF_INET, &node->destipaddr, destipaddr, INET_ADDRSTRLEN);
 
-    int srcport = ntohs(node->srcport);
-    int destport = ntohs(node->destport);
+  int srcport = ntohs(node->srcport);
+  int destport = ntohs(node->destport);
 
-    char rule[RULE_MAX_SIZE];
-    strcpy(rule, get_rule_name(node->rule));
+  char rule[RULE_MAX_SIZE];
+  strcpy(rule, get_rule_name(node->rule));
 
-    char rule_info[RULE_INFO_MAX_SIZE];
-    sprintf(rule_info, "%2d: %10s %16s %16s %9d %9d", \
-    idx, rule, srcipaddr, destipaddr, srcport, destport);
+  char rule_info[RULE_INFO_MAX_SIZE];
+  sprintf(rule_info, "%2d: %10s %16s %16s %9d %9d",
+          idx, rule, srcipaddr, destipaddr, srcport, destport);
 
-    strcpy(table[idx], rule_info);
+  strcpy(table[idx], rule_info);
 }
 
 int nflite_get_rules_counter()
 {
-    return rules_counter;
-}
-
-char** nflite_listall(void)
-{
-    chain *head = chain_head->next;
-    char **table = (char **)malloc(rules_counter * sizeof(char *));
-
-    for (int i = 0; i < rules_counter; i++)
-    {
-        table[i] = (char *)malloc(RULE_INFO_MAX_SIZE * sizeof(char));
-    }
-
-    int idx = 0;
-    while (head != NULL)
-    {
-        get_rule_info(head, table, idx);
-        head = head->next;
-        idx++;
-    }
-
-    return table;
+  return rules_counter;
 }
